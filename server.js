bdd829ded7644c32c4857ce51ac77021eab62046
nhonoree <honoreniyirema@gmail.com>
2025-03-30 15:23:14 -0700

    updates

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route for the homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(8000, () => console.log('Server running at http://localhost:8000'));
